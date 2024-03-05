2b5b786d35a3451fb08626b6166bbcff63d02b89
Gayathri-Nettem <nettemgayathri23@gmail.com>
2024-03-06 00:14:30 +0530

    pipelines main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I am Nettem Gayathri.\n";
+  cout << "My SRN is PES1UG21CS378.\n";
+  cout << "Pipeline created.\n";
+  return 0;
+}
